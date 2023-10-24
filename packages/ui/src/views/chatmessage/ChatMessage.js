a866851b3d6dfcd775f95780f68540f87cc21b20
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2023-10-24 21:28:32 +0500

    italy lang

--- packages/ui/src/views/chatmessage/ChatMessage.js
+++ packages/ui/src/views/chatmessage/ChatMessage.js
@@ -42,7 +42,7 @@ export const ChatMessage = ({ open, chatflowid, isDialog }) => {
     const [loading, setLoading] = useState(false)
     const [messages, setMessages] = useState([
         {
-            message: 'Hi there! How can I help?',
+            message: 'Ciao! Come posso aiutare?',
             type: 'apiMessage'
         }
     ])
@@ -148,7 +148,7 @@ export const ChatMessage = ({ open, chatflowid, isDialog }) => {
         try {
             const params = {
                 question: userInput,
-                history: messages.filter((msg) => msg.message !== 'Hi there! How can I help?')
+                history: messages.filter((msg) => msg.message !== 'Ciao! Come posso aiutare?')
             }
             if (isChatFlowAvailableToStream) params.socketIOClientId = socketIOClientId
 
@@ -265,7 +265,7 @@ export const ChatMessage = ({ open, chatflowid, isDialog }) => {
             setLoading(false)
             setMessages([
                 {
-                    message: 'Hi there! How can I help?',
+                    message: 'Ciao! Come posso aiutare?',
                     type: 'apiMessage'
                 }
             ])
